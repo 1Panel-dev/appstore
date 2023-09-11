c75ccdb14d976690251ee4de36f314cac78518e4
zhengkunwang223 <zhengkun@fit2cloud.com>
2023-09-11 17:54:21 +0800

    fix: 解决部分 php 版本构建失败的问题

--- apps/php5/5.5.38/build/php/extensions/install-composer.sh
+++ apps/php5/5.5.38/build/php/extensions/install-composer.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# The latest mirror's composer version only support for PHP 7.2.5
+# The latest mirror's composer version only support for PHP 5.6.0
 # And if your PHP version is lesser than that, will be download supported version.
-supportLatest=$(php -r "echo version_compare(PHP_VERSION, '7.2.5', '>');")
+supportLatest=$(php -r "echo version_compare(PHP_VERSION, '5.5.0', '>');")
 
 if [ "$supportLatest" -eq "1" ]; then
     curl -o /usr/bin/composer https://mirrors.aliyun.com/composer/composer.phar \
