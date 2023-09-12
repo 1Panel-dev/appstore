7536cf8e575f8b2ba206f09dbe8e0345851fc921
zhengkunwang223 <zhengkun@fit2cloud.com>
2023-09-12 12:54:19 +0800

    fix: 解决 PHP 5.5.38 安装 composer 失败的问题

--- apps/php5/5.5.38/build/php/extensions/install-composer.sh
+++ apps/php5/5.5.38/build/php/extensions/install-composer.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-# The latest mirror's composer version only support for PHP 5.6.0
+# The latest mirror's composer version only support for PHP 7.2.5
 # And if your PHP version is lesser than that, will be download supported version.
-supportLatest=$(php -r "echo version_compare(PHP_VERSION, '5.5.0', '>');")
+supportLatest=$(php -r "echo version_compare(PHP_VERSION, '7.2.5', '>');")
 
 if [ "$supportLatest" -eq "1" ]; then
     curl -o /usr/bin/composer https://mirrors.aliyun.com/composer/composer.phar \
     && chmod +x /usr/bin/composer
 else
-    curl -o /tmp/composer-setup.php https://getcomposer.org/installer  \
-    && php /tmp/composer-setup.php --install-dir=/tmp \
-    && mv /tmp/composer.phar /usr/bin/composer \
+    curl  -ksS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer --disable-tls \
     && chmod +x /usr/bin/composer \
     && rm -rf /tmp/composer-setup.php
 fi
