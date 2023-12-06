f4fa2951e0b071cf69b813ef1f1992c1790ee708
zhengkunwang223 <zhengkun@fit2cloud.com>
2023-12-06 17:28:56 +0800

    feat: 修改 PHP 启动脚本

--- apps/php7/7.4.33/supervisor/run.sh
+++ apps/php7/7.4.33/supervisor/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 if command -v supervisord > /dev/null 2>&1; then
+    php-fpm -y /usr/local/etc/php-fpm.conf &
     supervisord -c /etc/supervisord.conf
+else
+   php-fpm -y /usr/local/etc/php-fpm.conf
 fi
