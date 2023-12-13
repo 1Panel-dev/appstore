8ffac42da5f16596e068472bc2ba22350ecf9a4a
zhengkunwang223 <zhengkun@fit2cloud.com>
2023-12-13 12:23:59 +0800

    feat: 取消在 PHP 中安装 supervisor

--- apps/php7/7.2.34/supervisor/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if command -v supervisord > /dev/null 2>&1; then
-    supervisord -c /etc/supervisord.conf
-fi
--- apps/php8/8.0.30/supervisor/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if command -v supervisord > /dev/null 2>&1; then
-    php-fpm -y /usr/local/etc/php-fpm.conf &
-    supervisord -c /etc/supervisord.conf
-else
-   php-fpm -y /usr/local/etc/php-fpm.conf
-fi
