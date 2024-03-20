17017664e3410be7af24fee4b501b4305b0ccd4c
萌森 <qyg2297248353@163.com>
2024-03-20 15:03:10 +0800

    Update Emby Config 允许修改容器内服务的端口，解决主机模式下端口冲突
    支持升级,如需修改端口请卸载重装即可，容器重建前提需要执行安装脚本，这一点未确认

--- apps/emby/4.9.0.10/scripts/init.sh
+++ apps/emby/4.9.0.10/scripts/init.sh
@@ -5,8 +5,21 @@ if [[ -f ./.env ]]; then
 
   mkdir -p "$EMBY_ROOT_PATH"
 
-  mkdir -p "$EMBY_ROOT_PATH/data"
+  mkdir -p "$EMBY_ROOT_PATH/config"
+  mkdir -p "$EMBY_ROOT_PATH/config/config"
   mkdir -p "$EMBY_ROOT_PATH/mnt"
+
+  if [ ! -f "$EMBY_ROOT_PATH/config/config/system.xml" ]; then
+    cp -f ./config/system.xml "$EMBY_ROOT_PATH/config/config/system.xml"
+    sed -i "s/{PANEL_APP_PORT_HTTP}/$PANEL_APP_PORT_HTTP/g" "$EMBY_ROOT_PATH/config/config/system.xml"
+    sed -i "s/{PANEL_APP_PORT_HTTPS}/$PANEL_APP_PORT_HTTPS/g" "$EMBY_ROOT_PATH/config/config/system.xml"
+  else
+    echo "system.xml already exists."
+    sed -i "s/<PublicPort>[0-9]\{1,5\}<\/PublicPort>/<PublicPort>$PANEL_APP_PORT_HTTP<\/PublicPort>/g" "$EMBY_ROOT_PATH/config/config/system.xml"
+    sed -i "s/<HttpServerPortNumber>[0-9]\{1,5\}<\/HttpServerPortNumber>/<HttpServerPortNumber>$PANEL_APP_PORT_HTTP<\/HttpServerPortNumber>/g" "$EMBY_ROOT_PATH/config/config/system.xml"
+    sed -i "s/<PublicHttpsPort>[0-9]\{1,5\}<\/PublicHttpsPort>/<PublicHttpsPort>$PANEL_APP_PORT_HTTPS<\/PublicHttpsPort>/g" "$EMBY_ROOT_PATH/config/config/system.xml"
+    sed -i "s/<HttpsPortNumber>[0-9]\{1,5\}<\/HttpsPortNumber>/<HttpsPortNumber>$PANEL_APP_PORT_HTTPS<\/HttpsPortNumber>/g" "$EMBY_ROOT_PATH/config/config/system.xml"
+  fi
 
   chmod -R $UID:$GID "$EMBY_ROOT_PATH"
 
--- apps/emby/4.9.0.10/scripts/upgrade.sh
+++ apps/emby/4.9.0.10/scripts/upgrade.sh
@@ -3,6 +3,14 @@
 if [[ -f ./.env ]]; then
   source .env
 
+  mkdir -p "$EMBY_ROOT_PATH"
+
+  mkdir -p "$EMBY_ROOT_PATH/config"
+  mkdir -p "$EMBY_ROOT_PATH/config/config"
+  mkdir -p "$EMBY_ROOT_PATH/mnt"
+
+  chmod -R $UID:$GID "$EMBY_ROOT_PATH"
+
   echo "Check Finish."
 
 else
