b53a25e205a72b58c3b9310a4170326432f80a31
萌森 <qyg2297248353@163.com>
2024-03-22 18:14:18 +0800

    Upgrade And Fix Minio 2024-03-15 1. 用户无法删除文件(影响API与WebUI) 2. 修复v1.10.2-lts图标变形 3. 新增配置参数，Nginx反代不受影响

--- apps/minio/2024-03-15/scripts/upgrade.sh
+++ apps/minio/2024-03-15/scripts/upgrade.sh
@@ -19,6 +19,24 @@ if [[ -f ./.env ]]; then
   else
     echo 'MINIO_BROWSER_LOGIN_ANIMATION="on"' >> ./.env
   fi
+
+  if grep -q "MINIO_IMAGE" ./.env; then
+    echo "MINIO_IMAGE 已存在"
+  else
+    echo 'MINIO_IMAGE="minio/minio:RELEASE.2024-03-15T01-07-19Z"' >> ./.env
+  fi
+
+  if grep -q "MINIO_BROWSER_REDIRECT_URL" ./.env; then
+    echo "MINIO_BROWSER_REDIRECT_URL 已存在"
+  else
+    echo 'MINIO_BROWSER_REDIRECT_URL="http://127.0.0.1:9001"' >> ./.env
+  fi
+
+  if grep -q "MINIO_SERVER_URL" ./.env; then
+    echo "MINIO_SERVER_URL 已存在"
+  else
+    echo 'MINIO_SERVER_URL="http://127.0.0.1:9000"' >> ./.env
+  fi
 else
   echo ".env 文件不存在"
 fi
