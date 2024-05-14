374300776aaec7cd5a010d5f1689bc5af0ed7f77
wanghe-fit2cloud <wanghe@fit2cloud.com>
2024-05-14 18:53:57 +0800

    fix: 修复了 Firefly III 选择 PostgreSQL 数据库时安装失败的问题。

--- apps/firefly-iii/6.1.15/scripts/init.sh
+++ apps/firefly-iii/6.1.15/scripts/init.sh
@@ -4,7 +4,7 @@ if [[ -f ./.env ]]; then
   if grep -q 'PANEL_DB_TYPE="mysql"' ./.env; then
     echo "PANEL_DB_TYPE 为 Mysql 数据库, 不作修改"
   else
-    sed -i 's/PANEL_DB_TYPE="postgres"/PANEL_DB_HOST="pgsql"/g' ./.env
+    sed -i 's/PANEL_DB_TYPE="postgres"/PANEL_DB_TYPE="pgsql"/g' ./.env
   fi
 else
   echo ".env 文件不存在"
