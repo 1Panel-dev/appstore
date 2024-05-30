9ac9bc64b46abdbfa1a27d0f2fe11f954b249094
zhengkunwang223 <zhengkun@fit2cloud.com>
2024-05-30 16:29:10 +0800

    feat: Node.js 增加 pnpm 支持

--- apps/node/18.20.3/run.sh
+++ apps/node/18.20.3/run.sh
@@ -6,6 +6,8 @@ if [[ "$PACKAGE_MANAGER" == "npm" ]]; then
     npm config set registry $CONTAINER_PACKAGE_URL
 elif [[ "$PACKAGE_MANAGER" == "yarn" ]]; then
     yarn config set registry $CONTAINER_PACKAGE_URL
+elif [[ "$PACKAGE_MANAGER" == "pnpm" ]]; then
+    pnpm config set registry $CONTAINER_PACKAGE_URL
 fi
 
 if [[ "$RUN_INSTALL" -eq "1" ]]; then
@@ -13,6 +15,8 @@ if [[ "$RUN_INSTALL" -eq "1" ]]; then
         npm install
     elif [[ "$PACKAGE_MANAGER" == "yarn" ]]; then
         yarn install
+    elif [[ "$PACKAGE_MANAGER" == "pnpm" ]]; then
+        pnpm install    
     else
         echo "未知的 PACKAGE_MANAGER: $PACKAGE_MANAGER"
         exit 1
@@ -20,8 +24,15 @@ if [[ "$RUN_INSTALL" -eq "1" ]]; then
 fi
 
 
-if [[ "$PACKAGE_MANAGER" == "npm" ]]; then
-    npm run $EXEC_SCRIPT
-elif [[ "$PACKAGE_MANAGER" == "yarn" ]]; then
-    yarn run $EXEC_SCRIPT
+if [[ "$CUSTOM_SCRIPT" -eq "1" ]]; then
+  $EXEC_SCRIPT
+else
+  if [[ "$PACKAGE_MANAGER" == "npm" ]]; then
+      npm run $EXEC_SCRIPT
+  elif [[ "$PACKAGE_MANAGER" == "yarn" ]]; then
+      yarn run $EXEC_SCRIPT
+  elif [[ "$PACKAGE_MANAGER" == "pnpm" ]]; then
+      pnpm run $EXEC_SCRIPT    
+  fi
 fi
+
