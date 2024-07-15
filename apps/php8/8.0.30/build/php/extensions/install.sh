17dfe9f6d6f3f3ab822c461205da1ed7f9e04803
zhengkunwang223 <zhengkun@fit2cloud.com>
2024-07-15 11:52:28 +0800

    feat: PHP 增加 zstd 扩展安装支持

--- apps/php8/8.0.30/build/php/extensions/install.sh
+++ apps/php8/8.0.30/build/php/extensions/install.sh
@@ -102,6 +102,11 @@ if [[ -z "${EXTENSIONS##*,maxminddb,*}" ]]; then
     echo "---------- Install maxminddb ----------"
 	  install-php-extensions maxminddb
 fi
+
+if [[ -z "${EXTENSIONS##*,zstd,*}" ]]; then
+    echo "---------- Install zstd ----------"
+	  install-php-extensions zstd
+fi
 # end
 
 
