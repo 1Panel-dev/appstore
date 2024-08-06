fdf95092109997e427ee391c2dfbd1a0076a8381
zhengkunwang223 <zhengkun@fit2cloud.com>
2024-08-06 11:17:38 +0800

    fix: 解决 ftp 扩展无法安装的问题

--- apps/php7/7.0.33/build/php/extensions/install.sh
+++ apps/php7/7.0.33/build/php/extensions/install.sh
@@ -109,6 +109,11 @@ if [[ -z "${EXTENSIONS##*,grpc,*}" ]]; then
 	  install-php-extensions grpc
 	  docker-php-ext-enable grpc
 fi
+
+if [[ -z "${EXTENSIONS##*,ftp,*}" ]]; then
+    echo "---------- Install ftp ----------"
+	  install-php-extensions ftp
+fi
 # end
 
 
