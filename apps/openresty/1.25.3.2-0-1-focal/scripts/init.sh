603a2008187adf9dffcaf158cdc6efd44082295d
zhengkunwang223 <zhengkun@fit2cloud.com>
2024-08-30 11:28:37 +0800

    feat: 修改 PHP 的配置文件

--- apps/openresty/1.25.3.2-0-1-focal/scripts/init.sh
+++ apps/openresty/1.25.3.2-0-1-focal/scripts/init.sh
@@ -7,3 +7,5 @@ sed -i -E "s/(listen[[:space:]]+)(443)([[:space:]]+ssl[[:space:]]+http2;)/\1${PA
 
 sed -i -E "s/(listen[[:space:]]+)(80)([[:space:]]*;)/\1${PANEL_APP_PORT_HTTP}\3/" conf/conf.d/default.conf
 
+
+
