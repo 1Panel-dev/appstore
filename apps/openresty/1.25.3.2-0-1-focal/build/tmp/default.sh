9b85535d8cbec578abfa821e44d41c494ecbaa6a
zhengkunwang223 <zhengkun@fit2cloud.com>
2025-01-06 17:04:20 +0800

    feat: change openresty sh

--- apps/openresty/1.25.3.2-0-1-focal/build/tmp/default.sh
+++ apps/openresty/1.25.3.2-0-1-focal/build/tmp/default.sh
@@ -1,5 +1,4 @@
-apt-get install -y --no-install-recommends libsqlite3-dev git  python2 libmaxminddb0\
-  && ln -s /usr/bin/python2 /usr/bin/python \
+apt-get install -y --no-install-recommends libsqlite3-dev git python3 automake autoconf libtool\
   && apt-get clean \
   && rm -rf /var/lib/apt/lists/* \
   && unzip /tmp/libmaxminddb.zip -d /tmp \
@@ -7,13 +6,13 @@ apt-get install -y --no-install-recommends libsqlite3-dev git  python2 libmaxmin
   && ./configure \
   && make  \
   && make install \
-  && unzip /tmp/libinjection-master.zip -d /tmp \
-  && cd /tmp/libinjection-master \
+  && unzip /tmp/libinjection-main.zip -d /tmp \
+  && cd /tmp/libinjection-main \
   && make all \
   && unzip /tmp/lsqlite3.zip -d /tmp \
   && cd /tmp/lsqlite3 \
   && make all \
   && mkdir -p /usr/local/openresty/1pwaf/libraries/ \
   && cp /usr/local/lib/libmaxminddb.so.0.0.7 /usr/local/openresty/1pwaf/libraries/libmaxminddb.so \
-  && cp /tmp/libinjection-master/src/libinjection.so /usr/local/openresty/1pwaf/libraries/libinjection.so \
+  && cp /tmp/libinjection-main/src/.libs/libinjection.so.1.2.9 /usr/local/openresty/1pwaf/libraries/libinjection.so \
   && cp /tmp/lsqlite3/lsqlite3.so /usr/local/openresty/luajit/lib/lua/5.1/lsqlite3.so 
