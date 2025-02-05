d791e2485d8f20eaf701402c4d0b361afba7b3ec
zhengkunwang223 <zhengkun@fit2cloud.com>
2025-02-05 16:09:38 +0800

    feat: update openresty init.sh

--- apps/openresty/1.27.1.1-1-focal/scripts/init.sh
+++ apps/openresty/1.27.1.1-1-focal/scripts/init.sh
@@ -2,10 +2,10 @@
 
 source ./.env
 
-sed -i -E "s/(listen[[:space:]]+)(80)([[:space:]]*;)/\1${PANEL_APP_PORT_HTTP}\3/" conf/conf.d/00.default.conf
-sed -i -E "s/(listen[[:space:]]+)(443)([[:space:]]+ssl[[:space:]]+http2;)/\1${PANEL_APP_PORT_HTTPS}\3/" conf/conf.d/00.default.conf
+sed -i -E "s/(listen[[:space:]]+)(80)([[:space:]]*;)/\1${PANEL_APP_PORT_HTTP}\3/" conf/default/00.default.conf
+sed -i -E "s/(listen[[:space:]]+)(443)([[:space:]]+ssl[[:space:]]+http2;)/\1${PANEL_APP_PORT_HTTPS}\3/" conf/default/00.default.conf
 
-sed -i -E "s/(listen[[:space:]]+)(80)([[:space:]]*;)/\1${PANEL_APP_PORT_HTTP}\3/" conf/conf.d/default.conf
+sed -i -E "s/(listen[[:space:]]+)(80)([[:space:]]*;)/\1${PANEL_APP_PORT_HTTP}\3/" conf/default/default.conf
 
 
 
