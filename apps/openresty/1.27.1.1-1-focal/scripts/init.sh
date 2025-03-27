d4b8e45790ddd9ef988c3519d34b0f45b088c78b
zhengkunwang223 <1paneldev@sina.com>
2025-03-27 16:35:19 +0800

    feat: Change init.sh script

--- apps/openresty/1.27.1.1-1-focal/scripts/init.sh
+++ apps/openresty/1.27.1.1-1-focal/scripts/init.sh
@@ -2,10 +2,7 @@
 
 source ./.env
 
-sed -i -E "s/(listen[[:space:]]+)(80)([[:space:]]*;)/\1${PANEL_APP_PORT_HTTP}\3/" conf/default/00.default.conf
-sed -i -E "s/(listen[[:space:]]+)(443)([[:space:]]+ssl[[:space:]]+http2;)/\1${PANEL_APP_PORT_HTTPS}\3/" conf/default/00.default.conf
+sed -i -E "s/(listen[[:space:]]+)80([[:space:]]*default_server;)/\1${PANEL_APP_PORT_HTTP}\2/" conf/default/00.default.conf
+sed -i -E "s/(listen[[:space:]]+)\[::]:80([[:space:]]*default_server;)/\1\[::]:${PANEL_APP_PORT_HTTP}\2/" conf/default/00.default.conf
 
 sed -i -E "s/(listen[[:space:]]+)(80)([[:space:]]*;)/\1${PANEL_APP_PORT_HTTP}\3/" conf/default/default.conf
-
-
-
