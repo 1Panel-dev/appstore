d8122fb8107062eadb1f206549aee68ce605ea71
CityFun <31820853+zhengkunwang223@users.noreply.github.com>
2025-06-09 22:44:08 +0800

    fix: fix issue with build with ngx_brotli failed (#4089)

--- apps/openresty/1.27.1.2-0-1-focal/build/tmp/default.sh
+++ apps/openresty/1.27.1.2-0-1-focal/build/tmp/default.sh
@@ -1,5 +1,4 @@
 apt-get install -y --no-install-recommends libsqlite3-dev git python3 automake autoconf libtool\
   && apt-get clean \
   && rm -rf /var/lib/apt/lists/* \
-  && luarocks install luafilesystem \
   && mkdir -p /usr/local/openresty/1pwaf/libraries
