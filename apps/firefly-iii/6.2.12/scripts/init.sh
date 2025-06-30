412059159dc3cf8d080a031ed3949ac94df02858
wanghe-fit2cloud <wanghe@fit2cloud.com>
2025-06-30 22:51:45 +0800

    fix: Fix the issue in the Firefly III initialization script

--- apps/firefly-iii/6.2.12/scripts/init.sh
+++ apps/firefly-iii/6.2.12/scripts/init.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-if [[ -f ./.env ]]; then
-  if grep -q 'PANEL_DB_TYPE="mysql"' ./.env; then
+ENV_FILE="./.env"
+
+if [[ -f "$ENV_FILE" ]]; then
+  if grep -q "^PANEL_DB_TYPE=['\"]mysql['\"]" "$ENV_FILE"; then
     echo "PANEL_DB_TYPE 为 Mysql 数据库, 不作修改"
   else
-    sed -i 's/PANEL_DB_TYPE="postgres"/PANEL_DB_TYPE="pgsql"/g' ./.env
+    sed -i -E "s/^PANEL_DB_TYPE=['\"]?postgres['\"]?/PANEL_DB_TYPE='pgsql'/" "$ENV_FILE"
+    echo "已将 PANEL_DB_TYPE 修改为 pgsql"
   fi
 else
   echo ".env 文件不存在"
