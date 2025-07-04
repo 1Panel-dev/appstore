08c2f95c3cdd38eb1de72ded3b613afbae7802f2
kyzhangs <kyzhangs@foxmail.com>
2025-07-04 17:29:10 +0800

    ✨ feat(app): add openlist app

---
+++ apps/openlist/4.0.7/scripts/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+[ -f ./.env ] && source ./.env
+
+# Default image name without pre-install environment
+OPENLIST_IMAGE=openlistteam/openlist:v4.0.7
+
+{
+  # Retain the original environment variables
+  grep -vE '^OPENLIST_IMAGE' ./.env 2>/dev/null
+
+  if [ -n "$PRE_INSTALLED" ]; then
+    # Change images with pre-install environment
+    OPENLIST_IMAGE=$OPENLIST_IMAGE-$PRE_INSTALLED
+  fi
+  echo "OPENLIST_IMAGE=${OPENLIST_IMAGE}"
+  echo ""
+} > ./.env.tmp && mv ./.env.tmp ./.env
