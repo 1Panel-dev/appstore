2a33d791d59ae17c2ef6b02ebe096e7dabc63f1a
wanghe-fit2cloud <wanghe@fit2cloud.com>
2025-09-11 16:40:01 +0800

    feat: Support OpenList v4.1.2

--- apps/openlist/4.1.2/scripts/init.sh
+++ apps/openlist/4.1.2/scripts/init.sh
@@ -3,7 +3,7 @@
 [ -f ./.env ] && source ./.env
 
 # Default image name without pre-install environment
-OPENLIST_IMAGE=openlistteam/openlist:v4.1.1
+OPENLIST_IMAGE=openlistteam/openlist:v4.1.2
 
 {
   # Retain the original environment variables
