88ca697f1e1b0b7c6e991cb4e9f63796805d1611
mxtag <156078944+mxtag@users.noreply.github.com>
2025-10-09 11:28:57 +0800

    feat: Support OpenList v4.1.4 (#5206)

--- apps/openlist/4.1.4/scripts/init.sh
+++ apps/openlist/4.1.4/scripts/init.sh
@@ -3,7 +3,7 @@
 [ -f ./.env ] && source ./.env
 
 # Default image name without pre-install environment
-OPENLIST_IMAGE=openlistteam/openlist:v4.1.3
+OPENLIST_IMAGE=openlistteam/openlist:v4.1.4
 
 {
   # Retain the original environment variables
