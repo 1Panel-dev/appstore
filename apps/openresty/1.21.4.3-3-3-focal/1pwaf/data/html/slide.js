b841a3eee2b5bc059f20f59d3ac4cbe570d28d8d
蘭 <81747598+lan-yonghui@users.noreply.github.com>
2025-11-07 17:37:25 +0800

    style: Optimize the style of slider access in OpenResty (#5527)

--- apps/openresty/1.21.4.3-3-3-focal/1pwaf/data/html/slide.js
+++ apps/openresty/1.21.4.3-3-3-focal/1pwaf/data/html/slide.js
@@ -4,54 +4,69 @@ window.onload = function () {
         const dragBg = document.getElementById("dragBg");
         const dragText = document.getElementById("dragText");
         const dragHandler = document.getElementById("dragHandler");
-        const maxHandleOffset = dragContainer.clientWidth - dragHandler.clientWidth;
+        
+        let maxHandleOffset;
+        
         let isVertifySucc = false;
-
-        initDrag();
-
+        
         function initDrag() {
+            maxHandleOffset = dragContainer.clientWidth - dragHandler.clientWidth;
             dragText.textContent = "拖动滑块验证";
+            dragHandler.className = "dragHandlerDefault";
+            dragHandler.style.left = "0px";
+            dragBg.style.width = "0px";
+            
             dragHandler.addEventListener("mousedown", onDragStart);
             dragHandler.addEventListener("touchstart", onDragStart);
         }
-
-        function onDragStart(a) {
-            a.preventDefault();
-            if (a.type === "mousedown" || (a.type === "touchstart" && a.touches.length === 1)) {
+        
+        function onDragStart(e) {
+            e.preventDefault();
+            if (isVertifySucc) return;
+            
+            if (e.type === "mousedown" || (e.type === "touchstart" && e.touches.length === 1)) {
+                maxHandleOffset = dragContainer.clientWidth - dragHandler.clientWidth;
+                
                 document.addEventListener("mousemove", onDragMove);
                 document.addEventListener("touchmove", onDragMove);
                 document.addEventListener("mouseup", onDragEnd);
                 document.addEventListener("touchend", onDragEnd);
             }
         }
-
-        function onDragMove(a) {
+        
+        function onDragMove(e) {
             let clientX;
-            if (a.type === "mousemove") {
-                clientX = a.clientX;
-            } else if (a.type === "touchmove" && a.touches.length === 1) {
-                clientX = a.touches[0].clientX;
+            if (e.type === "mousemove") {
+                clientX = e.clientX;
+            } else if (e.type === "touchmove" && e.touches.length === 1) {
+                clientX = e.touches[0].clientX;
+            } else {
+                return;
             }
+            
             let containerOffsetX = clientX - dragContainer.getBoundingClientRect().left;
             let left = containerOffsetX - dragHandler.clientWidth / 2;
+            
             if (left < 0) {
                 left = 0;
             } else if (left > maxHandleOffset) {
                 left = maxHandleOffset;
             }
+            
             dragHandler.style.left = left + "px";
-            dragBg.style.width = dragHandler.style.left;
+            dragBg.style.width = left + dragHandler.clientWidth / 2 + "px";
         }
-
+        
         function onDragEnd() {
             document.removeEventListener("mousemove", onDragMove);
             document.removeEventListener("touchmove", onDragMove);
             document.removeEventListener("mouseup", onDragEnd);
             document.removeEventListener("touchend", onDragEnd);
-
+            
             if (!isVertifySucc) {
                 let left = dragHandler.offsetLeft;
-                if (left >= maxHandleOffset) {
+                
+                if (left >= maxHandleOffset - 1) {
                     verifySucc();
                 } else {
                     dragHandler.style.left = "0px";
@@ -59,15 +74,18 @@ window.onload = function () {
                 }
             }
         }
-
+        
         function verifySucc() {
             isVertifySucc = true;
             dragText.textContent = "验证通过";
-            dragText.style.color = "white";
-            dragHandler.setAttribute("class", "dragHandlerOkBg");
+            dragHandler.className = "dragHandlerSuccess";
+            
+            dragHandler.style.left = maxHandleOffset + "px";
+            dragBg.style.width = dragContainer.clientWidth + "px";
+            
             dragHandler.removeEventListener("mousedown", onDragStart);
             dragHandler.removeEventListener("touchstart", onDragStart);
-
+            
             let xhr = new XMLHttpRequest();
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4 && xhr.status === 200) {
@@ -78,5 +96,9 @@ window.onload = function () {
             xhr.open("GET", requestUrl, true);
             xhr.send();
         }
+        
+        window.addEventListener('resize', initDrag);
+        
+        initDrag();
     })();
 };
