d3dfede412c0ee961367f3344ca2e07cbc608b1d
NearlyHeadlessJack <wang@rjack.cn>
2025-12-30 16:11:24 +0800

    add wavelog

---
+++ apps/wavelog/2.2.1/scripts/init.sh
@@ -0,0 +1,8 @@
+mkdir -p ./data/wavelog-config
+mkdir -p ./data/wavelog-uploads
+mkdir -p ./data/wavelog-userdata
+
+
+sudo chmod -R 777 ./data/wavelog-config
+sudo chmod -R 777 ./data/wavelog-uploads
+sudo chmod -R 777 ./data/wavelog-userdata
