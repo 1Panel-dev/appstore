adf7ed7ecd8020bce7d659e94df1a86ce38511e0
wanghe-fit2cloud <wanghe@fit2cloud.com>
2026-07-06 11:45:45 +0800

    feat: support MariaDB for Forgejo

--- apps/forgejo/15.0.3/scripts/init.sh
+++ apps/forgejo/15.0.3/scripts/init.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-[ -f ./.env ] && source ./.env
+ENV_FILE="./.env"
+
+[ -f "${ENV_FILE}" ] && source "${ENV_FILE}"
+
+if [[ -f "${ENV_FILE}" ]] && grep -Eq "^PANEL_DB_TYPE=['\"]?mariadb['\"]?$" "${ENV_FILE}"; then
+    sed -i.bak -E "s/^PANEL_DB_TYPE=['\"]?mariadb['\"]?$/PANEL_DB_TYPE='mysql'/" "${ENV_FILE}"
+    rm -f "${ENV_FILE}.bak"
+fi
 
 # renovate: datasource=docker depName=forgejo/forgejo
 IMAGE=codeberg.org/forgejo/forgejo:15.0.3
