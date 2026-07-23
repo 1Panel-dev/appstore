dc1b179870c0450d6e0a6d26b2f46301b2087edb
zhengkunwang223 <1paneldev@sina.com>
2026-07-23 10:54:19 +0800

    feat: add dynamic build logic

--- apps/openresty/1.31.1.1-2-1-noble/scripts/upgrade.sh
+++ apps/openresty/1.31.1.1-2-1-noble/scripts/upgrade.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 NGINX_CONF="conf/nginx.conf"
+MODULE_INCLUDE="include /usr/local/openresty/nginx/conf/modules-enabled/*.conf;"
+
+mkdir -p modules conf/modules-enabled
+
+if [ ! -f "$NGINX_CONF" ]; then
+    echo "✗ failed:  $NGINX_CONF not found"
+    exit 1
+fi
+
+if ! grep -Fq "$MODULE_INCLUDE" "$NGINX_CONF"; then
+    sed -i "1i$MODULE_INCLUDE" "$NGINX_CONF"
+fi
 
 STREAM_BLOCK='stream {
     log_format streamlog '\''$remote_addr[$time_local] '\''
@@ -10,11 +22,6 @@ STREAM_BLOCK='stream {
                          
     include /usr/local/openresty/nginx/conf/stream.d/*.conf;
 }'
-
-if [ ! -f "$NGINX_CONF" ]; then
-    echo "✗ failed:  $NGINX_CONF not found"
-    exit 1
-fi
 
 if grep -q "stream[[:space:]]*{" "$NGINX_CONF"; then
     exit 0
