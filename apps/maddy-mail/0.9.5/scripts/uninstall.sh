00418f5591c1005d6efebbe9d13915a9fa641117
王贺 <wanghe@fit2cloud.com>
2026-08-20 18:23:38 +0800

    fix: support compose v2 in maddy-mail uninstall (#9149)

--- apps/maddy-mail/0.9.5/scripts/uninstall.sh
+++ apps/maddy-mail/0.9.5/scripts/uninstall.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker-compose down --volumes
+if docker compose version >/dev/null 2>&1; then
+    docker compose down --volumes
+elif docker-compose version >/dev/null 2>&1; then
+    docker-compose down --volumes
+else
+    echo "Docker Compose command not found" >&2
+    exit 127
+fi
